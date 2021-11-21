8bd45a04ed79dbd7641b44c8ee491ef3574de657
Jesse Langford <jesse@passkit.com>
2021-11-21 18:19:19 +0800

    test

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const app = express();
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.get("/users/api", (req, res) => {
-  console.log("test22");
+  console.log("test23");
   res.send(users);
 });
 
